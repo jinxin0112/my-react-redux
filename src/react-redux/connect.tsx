a4d769db7b0bc96b476dd9bbc535b08e17320ab3
jinxin0112 <m18508218948@163.com>
2019-08-09 11:20:20 +0800

    feat: connect subscribe

--- src/react-redux/connect.tsx
+++ src/react-redux/connect.tsx
@@ -16,44 +16,50 @@ type MapDispatchToProps =
 const connect = <S extends any>(
   mapStateToProps: MapStateToProps<S>,
   mapDispatchToProps: MapDispatchToProps
-) => (Element: React.ComponentType<any>) => (props: any) => {
-  const store = React.useContext(ReactReduxContext);
-  if (!store) {
-    throw new Error('no store');
-  }
-  const { getState, dispatch } = store;
-  const state = getState();
-  const connectProps = React.useMemo(() => {
-    const mapStateObject = mapStateToProps(state);
-    if (isPlainObject(mapDispatchToProps)) {
-      const mapDispatchObject = bindActionCreators(
-        mapDispatchToProps as ActionCreatorsMapObject,
-        dispatch
-      );
+) => (Element: React.ComponentType<any>) => {
+  return (props: any) => {
+    const store = React.useContext(ReactReduxContext);
+    if (!store) {
+      throw new Error('no store');
+    }
+    const { getState, dispatch, subscribe } = store;
+    const [state, setState] = React.useState(getState());
+    React.useEffect(() => {
+      subscribe(() => {
+        setState(getState());
+      });
+    }, []);
+    const connectProps = React.useMemo(() => {
+      const mapStateObject = mapStateToProps(state);
+      if (isPlainObject(mapDispatchToProps)) {
+        const mapDispatchObject = bindActionCreators(
+          mapDispatchToProps as ActionCreatorsMapObject,
+          dispatch
+        );
+        return {
+          ...props,
+          ...mapStateObject,
+          ...mapDispatchObject,
+          dispatch
+        };
+      }
+      if (typeof mapDispatchToProps === 'function') {
+        const mapDispatchObject = mapDispatchToProps(dispatch, props);
+        return {
+          ...props,
+          ...mapStateObject,
+          ...mapDispatchObject,
+          dispatch
+        };
+      }
       return {
         ...props,
         ...mapStateObject,
-        ...mapDispatchObject,
         dispatch
       };
-    }
-    if (typeof mapDispatchToProps === 'function') {
-      const mapDispatchObject = mapDispatchToProps(dispatch, props);
-      return {
-        ...props,
-        ...mapStateObject,
-        ...mapDispatchObject,
-        dispatch
-      };
-    }
-    return {
-      ...props,
-      ...mapStateObject,
-      dispatch
-    };
-  }, [state, dispatch, props]);
-
-  return <Element {...connectProps} />;
+    }, [state, dispatch, props, mapDispatchToProps, mapStateToProps]);
+    return <Element {...connectProps} />;
+  };
 };
 
 export default connect;
